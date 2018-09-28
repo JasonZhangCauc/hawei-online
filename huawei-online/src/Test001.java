ed4a8ef784631feda132e315634c913658f972a1
JasonZhangCauc <hasp_Jason@163.com>
2018-09-28 21:47:51 +0800

    Signed-off-by: JasonZhangCauc <hasp_Jason@163.com>

--- huawei-online/src/Test001.java
+++ huawei-online/src/Test001.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 /**
  * 
- * 
- * Test001
- * 创建人:JasonZhang
- * 时间：2018年9月27日-下午7:55:57 
- * @version 1.0.0
+ * 题目描述
+	计算字符串最后一个单词的长度，单词以空格隔开。 
+ * 输入描述:
+	一行字符串，非空，长度小于5000。
+
+ * 输出描述:
+	整数N，最后一个单词的长度。
+
+ * 示例1
+	输入
+	hello world
+	输出
+	5
  *
  */
 public class Test001 {
-
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		String str = scanner.nextLine();
+		String arrays[]=str.split(" ");
+		String end=arrays[arrays.length-1];
+		System.out.println(end.length());
+	}
 }
